cb2937ced853e5ceb4bdf03bf3faa7dbcc2af585
Diego Lauar <diego_pktd@hotmail.com>
2025-02-11 10:05:23 -0300

    Migrado para TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import { Post } from './components/Post'
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
-import './global.css'
-import style from './App.module.css'
+import { Header } from './components/Header';
+import { Post, PostType } from './components/Post'
+import { Sidebar } from './components/Sidebar';
 
+import styles from './App.module.css';
 
-const posts = [
+import './global.css';
+
+const posts: PostType[] = [
   {
     id: 1,
     author: {
       avatarUrl: 'https://github.com/diegolauar.png',
-      name: 'Diego Lauar',
-      role: 'Dev Backend at Semantix'
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2024-05-06 20:00:00'),
+    publishedAt: new Date('2022-05-03 20:00:00'),
   },
   {
     id: 2,
@@ -32,33 +33,28 @@ const posts = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2024-05-07 20:00:00'),
+    publishedAt: new Date('2022-05-10 20:00:00'),
   },
 ];
 
-
 export function App() {
-
   return (
     <div>
       <Header />
 
-      <div className={style.wrapper}>
+      <div className={styles.wrapper}>
         <Sidebar />
         <main>
           {posts.map(post => {
             return (
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             )
           })}
         </main>
       </div>
     </div>
-
   )
 }
